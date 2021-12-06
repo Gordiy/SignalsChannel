3f86180dce44dd1d071896da20690cc0f1344ca8
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-12-06 19:33:30 +0200

    Enh: return currency as string not list

--- Parser/GoodwillParser.py
+++ Parser/GoodwillParser.py
@@ -10,7 +10,7 @@ class GoodwillParser(BaseParser):
     def parse_message(self, text):
         stop = ("❌", "✅")
         if stop[0] not in text or stop[1] not in text:
-            self.__currency = re.findall(r"\b\w{3} / \w{3}\b", text)
+            self.__currency = re.findall(r"\b\w{3} / \w{3}\b", text)[0]
             self.__time = 5
             offer_act = self.__action.action(text)
             self.__action = offer_act
