a7e80119a69bff1f9cee06d5fbf6d5332f95f5fc
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-12-06 19:17:52 +0200

    Enh: parse good will bot message

--- Parser/GoodwillParser.py
+++ Parser/GoodwillParser.py
@@ -6,9 +6,8 @@ from .BaseParser import BaseParser
 class GoodwillParser(BaseParser):
     def parse_message(self, text):
         stop = ("❌", "✅")
-        action = ("BUY", "SELL")
         if stop[0] not in text or stop[1] not in text:
-            self.__currency = re.findall(r"\b\w{3}/\w{3}\b", text)
+            self.__currency = re.findall(r"\b\w{3} / \w{3}\b", text)
             self.__time = 5
-            if action[0] in text:
-                self.__action = None
+            offer_act = self.__action.action(text)
+            self.__action = offer_act
