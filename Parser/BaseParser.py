ed8d31bf7eb826a82b1fff9c0a41d60b560114c9
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-12-06 18:52:01 +0200

    Enh: return properties

--- Parser/BaseParser.py
+++ Parser/BaseParser.py
@@ -1,8 +1,21 @@
+from .Action import Action
+
+
 class BaseParser:
     def __init__(self):
         self.__time = None
         self.__currency = None
-        self.__action = None
+        self.__action = Action("").action
+
+        self.__offer = {}
 
     def parse_message(self, text):
         pass
+
+    @property
+    def time(self):
+        return self.time
+
+    @property
+    def offer(self):
+        return self.__offer
