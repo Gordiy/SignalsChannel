b3a9e9c893e8e2803dd026d81e6cd55c244ee90c
hordii.rushynets <hordii.rushynets@globallogic.com>
2022-01-30 00:05:01 +0200

    Fixing

--- Message/PocketOptionMessage.py
+++ Message/PocketOptionMessage.py
@@ -4,4 +4,4 @@ from .BaseMessage import Message
 class PocketOptionMessage(Message):
     """Define offer message for Pocket Option"""
     def create_message(self, offer, addition_text):
-        return f"Pocket Option - 💰{offer['currency'].upper()} - {offer['time']} мин. {offer['action']}}"
+        return f"Pocket Option - 💰{offer['currency'].upper()} - {offer['time']} мин. {offer['action']}"
--- Parser/StatisticParser.py
+++ Parser/StatisticParser.py
@@ -6,17 +6,15 @@ class StatisticParser(BaseParser):
         words = ("❌", "✅")
         if words[0] in text:
             return False
-        else:
+        elif words[1] in text:
             return True
 
     def __smart_trader_statistic(self, text):
         words = ("плюс", "минус")
         if words[0] in text:
             return True
-        elif words[1]:
+        elif words[1] in text:
             return False
-        else:
-            return None
 
     def parse_message(self, text, trader):
         if trader == "smart_trader":
